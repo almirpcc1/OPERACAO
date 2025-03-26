e3c95876e7c7bbfbb31230b74aa4179252640f3a
almirpcc1 <almirpcc1@gmail.com>
2025-03-26 14:01:37 +0000

    OK

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ def payment():
 
         # Define o valor baseado na origem
         if source == 'insurance':
-            amount = 27.60  # Valor fixo para o seguro
+            amount = 37.60  # Valor fixo para o seguro
         elif source == 'index':
             amount = 142.83
         else:
